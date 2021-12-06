8845a8065248515fd5afb7df2caab273948fd205
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-06 17:12:39 +0530

    Advent of Code: Update Day 6

--- Other Contests/Advent Of Code 2021/Day6/Day6Q1.py
+++ Other Contests/Advent Of Code 2021/Day6/Day6Q1.py
@@ -1,22 +1,13 @@
-from sys import setrecursionlimit
-setrecursionlimit(int(1e9))
-
-memo = {}
-def solve(i, n):
-	if (i, n) in memo:
-		return memo[(i, n)]
-	if n == 0:
-		return 1
-	if i == 0:
-		memo[i, n] = solve(6, n - 1) + solve(8, n - 1)
-		return memo[(i, n)]
-	memo[(i, n)] = solve(i - 1, n - 1)
-	return memo[(i, n)]
-
 inputFile = open('input.txt', 'r')
 l = list(map(int, inputFile.read().splitlines()[0].split(',')))
-res = 0
-for i in l:
-	res += solve(i, 256)
-print(res)
+for _ in range(80):
+	a = []
+	for i in l:
+		if i == 0:
+			a.append(6)
+			a.append(8)
+		else:
+			a.append(i - 1)
+	l = a
+print(len(l))
 inputFile.close()
---
+++ Other Contests/Advent Of Code 2021/Day6/Day6Q2.py
@@ -0,0 +1,22 @@
+from sys import setrecursionlimit
+setrecursionlimit(int(1e9))
+
+memo = {}
+def solve(i, n):
+	if (i, n) in memo:
+		return memo[(i, n)]
+	if n == 0:
+		return 1
+	if i == 0:
+		memo[i, n] = solve(6, n - 1) + solve(8, n - 1)
+		return memo[(i, n)]
+	memo[(i, n)] = solve(i - 1, n - 1)
+	return memo[(i, n)]
+
+inputFile = open('input.txt', 'r')
+l = list(map(int, inputFile.read().splitlines()[0].split(',')))
+res = 0
+for i in l:
+	res += solve(i, 256)
+print(res)
+inputFile.close()
