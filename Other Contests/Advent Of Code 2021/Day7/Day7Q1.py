5f5161e1f151b66899e611d88da5c51b2e2677e9
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-07 10:37:35 +0530

    Advent of Code: Day 7

--- Other Contests/Advent Of Code 2021/Day7/Day7Q1.py
+++ Other Contests/Advent Of Code 2021/Day7/Day7Q1.py
@@ -1,4 +1,10 @@
 inputFile = inputFile = open('input.txt', 'r')
-data = inputFile.read().splitlines()
-
+l = list(map(int, inputFile.read().splitlines()[0].split(',')))
+res = int(1e10)
+for i in range(max(l)):
+	c = 0
+	for j in l:
+		c += abs(i - j)
+	res = min(res, c)
+print(res)
 inputFile.close()
