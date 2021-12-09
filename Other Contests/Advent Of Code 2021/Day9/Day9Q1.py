3107a58c8cace391a28ec50beac3880cc4876767
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-09 11:05:45 +0530

    Advent of Code: Day 8

--- Other Contests/Advent Of Code 2021/Day9/Day9Q1.py
+++ Other Contests/Advent Of Code 2021/Day9/Day9Q1.py
@@ -1,4 +1,16 @@
 inputFile = inputFile = open('input.txt', 'r')
 data = inputFile.read().splitlines()
 
+a = [[100] * (len(data[0]) + 2) for i in range(len(data) + 2)]
+for i in range(100):
+	for j in range(100):
+		a[i + 1][j + 1] = int(data[i][j])
+
+res = 0
+for i in range(1, len(a) - 1):
+	for j in range(1, len(a[i]) - 1):
+		if a[i][j] < min(a[i - 1][j], a[i + 1][j], a[i][j + 1], a[i][j - 1]):
+			res += a[i][j] + 1
+print(res)
+
 inputFile.close()
--- Other Contests/Advent Of Code 2021/Day9/Day9Q2.py
+++ Other Contests/Advent Of Code 2021/Day9/Day9Q2.py
@@ -1,4 +1,33 @@
 inputFile = inputFile = open('input.txt', 'r')
 data = inputFile.read().splitlines()
 
+def dfs(i, j):
+	if a[i][j] == 100 or a[i][j] == 9:
+		return 0
+
+	if visited[i][j]:
+		return 0
+	
+	visited[i][j] = True
+	ans = 1
+	for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+		ans += dfs(x, y)
+	
+	return ans
+
+a = [[100] * (len(data[0]) + 2) for i in range(len(data) + 2)]
+for i in range(1, 101):
+	for j in range(1, 101):
+		a[i][j] = int(data[i - 1][j - 1])
+
+visited = [[False] * len(a[i]) for i in range(len(a))]
+res = []
+for i in range(len(a)):
+	for j in range(len(a[i])):
+		if not visited[i][j] and a[i][j] not in {9, 100}:
+			x = dfs(i, j)
+			res.append(x)
+res.sort()
+print(res[-3] * res[-2] * res[-1])
+
 inputFile.close()
