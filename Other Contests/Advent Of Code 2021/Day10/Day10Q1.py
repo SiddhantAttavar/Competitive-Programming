535b43361b3258e420309c304604b363ceb42307
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-10 11:47:45 +0530

    Advent of Code: Day 10

--- Other Contests/Advent Of Code 2021/Day10/Day10Q1.py
+++ Other Contests/Advent Of Code 2021/Day10/Day10Q1.py
@@ -1,4 +1,31 @@
 inputFile = inputFile = open('input.txt', 'r')
 data = inputFile.read().splitlines()
 
+score = {
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137
+}
+
+match = {
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<'
+}
+
+res = 0
+for j, l in enumerate(data):
+	curr = []
+	for i, c in enumerate(l):
+		if c in score:
+			if not curr or curr.pop() != match[c]:
+				res += score[c]
+				break
+		else:
+			curr.append(c)
+
+print(res)
+
 inputFile.close()
--- Other Contests/Advent Of Code 2021/Day10/Day10Q2.py
+++ Other Contests/Advent Of Code 2021/Day10/Day10Q2.py
@@ -1,4 +1,39 @@
 inputFile = inputFile = open('input.txt', 'r')
 data = inputFile.read().splitlines()
 
+score = {
+	'(': 1,
+	'[': 2,
+	'{': 3,
+	'<': 4
+}
+
+match = {
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<'
+}
+
+res = []
+for l in data:
+	curr = []
+	for c in l:
+		if c in score:
+			curr.append(c)
+		elif not curr or curr.pop() != match[c]:
+				break
+	else:
+		lineScore = 0
+		for c in curr[::-1]:
+			lineScore *= 5
+			lineScore += score[c]
+		res.append(lineScore)
+
+res.sort()
+if len(res) % 2 == 0:
+	print((res[len(res) // 2] + res[len(res) // 2 - 1]) // 2)
+else:
+	print(res[len(res) // 2])
+
 inputFile.close()
