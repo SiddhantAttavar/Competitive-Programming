4e7cbfa7731c0c66db37e0c0529b54796233b254
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-11 15:31:14 +0530

    Update Advent of Code templates

--- Other Contests/Advent Of Code 2021/Template.ps1
+++ Other Contests/Advent Of Code 2021/Template.ps1
@@ -2,6 +2,14 @@
 # Note remove this comment before running the command
 # This will replace existing files
 
+$template = @'
+from aocd import get_data, submit
+data = get_data(day = ${day}, year = ${year}).splitlines()
+
+print(res)
+# submit(res, part = ${part}, day = ${day}, year = ${year})
+'@
+
 for ($i = 1; $i -le 25; $i++) {
 	# If the folder exists, continue
 	if (Test-Path -Path $PSScriptRoot\Day$i) {
@@ -16,14 +24,14 @@ for ($i = 1; $i -le 25; $i++) {
 	for ($j = 1; $j -le 2; $j++) {
 		# Create file
 		$file = "$PSScriptRoot\Day$i\Day$i`Q$j`.py"
-		$alpha = [char](96 + $j)
+		$day = "$i"
+		$year = "$PSScriptRoot".Split("/")[-1].Split(" ")[-1]
+		$part = [char](96 + $j)
 		New-Item -Type File -Path $file | Out-Null
 
-		# Day i, Part 1
-		echo "from aocd import get_data, submit" >> $file
-		echo "data = get_data(day = $i, year = 2021).splitlines()" >> $file
-		echo "" >> $file
-		echo "print(res)" >> $file
-		echo "#submit(res, part = '$alpha', day = $i, year = 2021)" >> $file
+		# Day i, Part j
+		$FileContent = $ExecutionContext.InvokeCommand.ExpandString($template)
+		$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+		[System.IO.File]::WriteAllLines($file, $FileContent, $Utf8NoBomEncoding)
 	}
 }
