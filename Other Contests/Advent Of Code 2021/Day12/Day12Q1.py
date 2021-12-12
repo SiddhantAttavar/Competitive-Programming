473ac13d733850f08ba742b3653d228ee9e2f420
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-12 11:00:10 +0530

    Advent of Code: Day 12

--- Other Contests/Advent Of Code 2021/Day12/Day12Q1.py
+++ Other Contests/Advent Of Code 2021/Day12/Day12Q1.py
@@ -1,5 +1,36 @@
 from aocd import get_data, submit
 data = get_data(day = 12, year = 2021).splitlines()
 
+from sys import setrecursionlimit
+setrecursionlimit(int(1e9))
+
+def dfs(u):
+	global res
+	if u == 'end':
+		res += 1
+	
+	visited[u] = True
+	for v in graph[u]:
+		if not visited[v] or v.isupper():
+			dfs(v)
+	
+	visited[u] = False
+
+graph = {}
+
+for l in data:
+	u, v = l.split('-')
+	if u not in graph:
+		graph[u] = []
+	if v not in graph:
+		graph[v] = []
+	graph[u].append(v)
+	graph[v].append(u)
+
+visited = {i: False for i in graph.keys()}
+visited['start'] = True
+res = 0
+dfs('start')
+
 print(res)
-# submit(res, part = a, day = 12, year = 2021)
+# submit(res, part = 'a', day = 12, year = 2021)
--- Other Contests/Advent Of Code 2021/Day12/Day12Q2.py
+++ Other Contests/Advent Of Code 2021/Day12/Day12Q2.py
@@ -1,5 +1,43 @@
 from aocd import get_data, submit
 data = get_data(day = 12, year = 2021).splitlines()
 
+from sys import setrecursionlimit
+setrecursionlimit(int(1e9))
+
+def dfs(u, b):
+	global res
+	if u == 'end':
+		res += 1
+		return
+	
+	visited[u] += 1
+
+	for v in graph[u]:
+		if v.isupper():
+			dfs(v, b)
+		elif visited[v] == 0:
+			dfs(v, b)
+		elif visited[v] == 1 and not b:
+			dfs(v, True)
+	
+	visited[u] -= 1
+
+graph = {}
+
+for l in data:
+	u, v = l.split('-')
+	if u not in graph:
+		graph[u] = []
+	if v not in graph:
+		graph[v] = []
+	
+	graph[u].append(v)
+	graph[v].append(u)
+
+visited = {i: 0 for i in graph.keys()}
+visited['start'] = 2
+res = 0
+dfs('start', False)
+
 print(res)
-# submit(res, part = b, day = 12, year = 2021)
+# submit(res, part = 'b', day = 12, year = 2021)
