2301ad38031b523be70825ee4bc01e5b20a67343
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-14 12:01:01 +0530

    Advent of Code: Day 14

--- Other Contests/Advent Of Code 2021/Day14/Day14Q1.py
+++ Other Contests/Advent Of Code 2021/Day14/Day14Q1.py
@@ -1,5 +1,49 @@
 from aocd import get_data, submit
 data = get_data(day = 14, year = 2021).splitlines()
+# data = '''NNCB
+
+# CH -> B
+# HH -> N
+# CB -> H
+# NH -> C
+# HB -> C
+# HC -> B
+# HN -> C
+# NN -> C
+# BH -> H
+# NC -> B
+# NB -> B
+# BN -> B
+# BB -> N
+# BC -> B
+# CC -> N
+# CN -> C'''.splitlines()
+
+x = data[0]
+m = [l.split(' -> ') for l in data[2:]]
+m = {i[0]: i[0][0] + i[1] for i in m}
+
+j = 0
+while j < 10:
+	n = ''
+	for i in range(1, len(x)):
+		if x[i - 1] + x[i] in m:
+			n += m[x[i - 1] + x[i]]
+			i += 1
+		else:
+			n += x[i - 1]
+		i += 1
+	n += x[-1]
+	x = n
+	j += 1
+
+freq = {}
+for i in x:
+	if i not in freq:
+		freq[i] = 0
+	freq[i] += 1
+
+res = max(freq.values()) - min(freq.values())
 
 print(res)
 # submit(res, part = 'a', day = 14, year = 2021)
--- Other Contests/Advent Of Code 2021/Day14/Day14Q2.py
+++ Other Contests/Advent Of Code 2021/Day14/Day14Q2.py
@@ -1,5 +1,69 @@
 from aocd import get_data, submit
 data = get_data(day = 14, year = 2021).splitlines()
+# data = '''NNCB
+
+# CH -> B
+# HH -> N
+# CB -> H
+# NH -> C
+# HB -> C
+# HC -> B
+# HN -> C
+# NN -> C
+# BH -> H
+# NC -> B
+# NB -> B
+# BN -> B
+# BB -> N
+# BC -> B
+# CC -> N
+# CN -> C'''.splitlines()
+
+from collections import defaultdict
+
+q = data[0]
+q = [ord(i) - ord('A') for i in q]
+m = [l.split(' -> ') for l in data[2:]]
+
+d = [[-1] * 26 for _ in range(26)]
+for x, y in m:
+	a = ord(x[0]) - ord('A')
+	b = ord(x[1]) - ord('A')
+	c = ord(y) - ord('A')
+
+	d[a][b] = c
+
+dp = [[[defaultdict(lambda: 0) for k in range(26)] for j in range(26)] for i in range(40)]
+for i in range(26):
+	for j in range(26):
+		dp[0][i][j][i] = 1
+		dp[0][i][j][j] += 1
+		if d[i][j] != -1:
+			dp[0][i][j][d[i][j]] += 1
+
+for s in range(1, 40):
+	for i in range(26):
+		for j in range(26):
+			if d[i][j] != -1:
+				k = d[i][j]
+				a = dp[s - 1][i][k]
+				b = dp[s - 1][k][j]
+				for x, y in list(a.items()) + list(b.items()):
+					dp[s][i][j][x] += y
+				dp[s][i][j][k] -= 1
+			else:
+				dp[s][i][j][i] = 1
+				dp[s][i][j][j] += 1
+
+fin = defaultdict(lambda: 0)
+for i in range(len(q) - 1):
+	for x, y in dp[-1][q[i]][q[i + 1]].items():
+		fin[x] += y
+
+for i in q[1: -1]:
+	fin[i] -= 1
+
+res = max(fin.values()) - min(fin.values())
 
 print(res)
 # submit(res, part = 'b', day = 14, year = 2021)
