614696ca1614d781dd893632f39e99af35127cbc
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-07 09:26:51 +0530

    Algorithm: KLengthSubsets

---
+++ Algorithms/KLengthSubsets.py
@@ -0,0 +1,28 @@
+a = list(map(int, input().split()))
+k = int(input())
+
+# Method 1
+print('Method 1')
+from itertools import combinations
+for subset in combinations(a, k):
+	print(*subset)
+
+# Method 2
+print('Method 2')
+
+def genSubsets(a, k):
+	if k == 0 or len(a) == 0 or k > len(a):
+		return []
+    
+	if k == 1:
+		return [[i] for i in a]
+
+	x = genSubsets(a[1:], k - 1)
+	y = genSubsets(a[1:], k)
+	for i in range(len(x)):
+		x[i] = [a[0]] + x[i]
+
+	return x + y
+
+for subset in genSubsets(a, k):
+	print(*subset)
