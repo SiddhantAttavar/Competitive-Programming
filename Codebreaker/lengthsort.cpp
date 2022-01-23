de0c6d321a6360a9643cf2816dc04e4c90f606fd
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-23 21:13:28 +0530

    Codebreaker: lengthsort

---
+++ Codebreaker/lengthsort.cpp
@@ -0,0 +1,26 @@
+// https://codebreaker.xyz/problem/lengthsort
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (int it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+	int n;
+	input(n);
+	vector<string> a(n);
+	arrPut(a);
+	stable_sort(a.begin(), a.end(), [](string x, string y) {
+		return x.size() > y.size();
+	});
+	for (string s : a) {
+		print(s);
+	}
+}
