4b0e7619ab977fc6e9419c72f5f132f65edac38d
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-23 21:03:13 +0530

    Codebreaker: contestcalc

---
+++ Codebreaker/contestcalc.cpp
@@ -0,0 +1,35 @@
+// https://codebreaker.xyz/problem/contestcalc
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (int it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+	int n;
+	input(n);
+	map<string, int> m;
+	while (n--) {
+		string s;
+		int x;
+		input(s, x);
+		m[s] = x;
+	}
+
+	int c;
+	input(c);
+	int sum = 0;
+	range(i, 0, c) {
+		string s;
+		input(s);
+		sum += m[s];
+	}
+	print(sum / c);
+}
