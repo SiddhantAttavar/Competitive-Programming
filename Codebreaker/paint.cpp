ff92ca5d6e4241a7b50e8fca130af3fc691b1619
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-23 21:09:33 +0530

    Codebreaker: paint

---
+++ Codebreaker/paint.cpp
@@ -0,0 +1,32 @@
+// https://codebreaker.xyz/problem/paint
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (int it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+	int n;
+	input(n);
+	vector<pair<int, int>> a(n);
+	range(i, 0, n) {
+		input(a[i].second);
+	}
+	range(i, 0, n) {
+		input(a[i].first);
+	}
+	sort(a.begin(), a.end());
+	reverse(a.begin(), a.end());
+	int res = 0;
+	range(i, 0, n) {
+		res += a[i].second + i * a[i].first;
+	}
+	print(res);
+}
