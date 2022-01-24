7c3587ffd9d41186b5045a615a356dfb59747fee
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-24 21:27:37 +0530

    Codebreaker: GSS

---
+++ Codebreaker/GSS.cpp
@@ -0,0 +1,30 @@
+// https://codebreaker.xyz/problem/GSS
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (int it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+	
+	int n;
+	input(n);
+	vector<int> a(n);
+	arrPut(a);
+	sort(a.begin(), a.end());
+	int res = 0;
+	for (int i = n - 1; i >= 3; i -= 4) {
+		res += a[i] + a[i - 1] + a[i - 2];
+	}
+	range(i, 0, n % 4) {
+		res += a[i];
+	}
+	print(res);
+}
