5e091a019a7eda4fe028bbdaedbe5382af473843
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-25 14:52:46 +0530

    Add Codebreaker solutions

---
+++ Codebreaker/lis.cpp
@@ -0,0 +1,33 @@
+// https://codebreaker.xyz/problem/lis
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (int it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+	
+	int n;
+	input(n);
+	vector<int> a(n);
+	arrPut(a);
+	
+	vector<int> curr = {a[0]};
+	range(i, 1, n) {
+		int x = lower_bound(curr.begin(), curr.end(), a[i]) - curr.begin();
+		if (x == curr.size()) {
+			curr.push_back(a[i]);
+		}
+		else {
+			curr[x] = a[i];
+		}
+	}
+	print(curr.size());
+}
