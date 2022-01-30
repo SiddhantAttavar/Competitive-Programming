c5b24ed5887c37821d1da0bc00e6a9f3822d24ab
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-01-30 14:22:10 +0530

    Codebreaker: oranges2

--- Codebreaker/oranges2.cpp
+++ Codebreaker/oranges2.cpp
@@ -3,26 +3,33 @@
 using namespace std;
 template<typename T> inline void input(T& inVar) {cin >> inVar;}
 template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
-template<typename T> inline void print(T outVar) {cout << outVar << endl;}
+template<typename T> inline void print(T outVar) {cout << outVar << "\n";}
 template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
 #define range(it, start, end) for (int it = start; it < end; it++)
 #define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
-#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << "\n"
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define int long long
 
-struct DSU {
-	vector<int> a;
+int n, m, q;
+const int MAXN = 1e5, MAXE = 3e5, MAXQ = 1e5;
+array<int, 3> edges[MAXE];
+vector<pair<int, int>> graph[MAXN];
+int p[MAXN][20], x[MAXN][20];
+int l[MAXN];
+int a[MAXN];
 
+struct DSU {
 	DSU(int n) {
-		a.resize(n, -1);
+		fill(a, a + n, -1);
 	}
 
 	int find(int u) {
 		if (a[u] < 0) {
 			return u;
 		}
-		return a[u] = find(a[u]);
+		a[u] = find(a[u]);
+		return a[u];
 	}
 
 	bool unite(int u, int v) {
@@ -43,45 +50,34 @@ struct DSU {
 	}
 };
 
-void dfs(vector<vector<pair<int, int>>> &graph, vector<int> &l, vector<vector<int>> &p, vector<vector<int>> &x, int u) {
+void dfs(int u) {
 	for (pair<int, int> e : graph[u]) {
 		int v, w;
 		tie(v, w) = e;
 		if (l[v] == -1) {
 			l[v] = l[u] + 1;
-			x[v][0] = w;
 			p[v][0] = u;
-			range(i, 1, p[v].size()) {
-				if (p[v][i - 1] == -1) {
-					break;
-				}
-				p[v][i] = p[p[v][i - 1]][i - 1];
-			
-			dfs(graph, l, p, x, v);
+			x[v][0] = w;
+			dfs(v);
 		}
 	}
 }
 
 int32_t main() {
 	setup();
-	
-	int n, m, q;
 	input(n, m, q);
 	
-	vector<tuple<int, int, int>> edges(m);
 	range(i, 0, m) {
-		int u, v, w;
-		input(u, v, w);
-		edges[i] = {w, u, v};
+		input(edges[i][0], edges[i][1], edges[i][2]);
 	}
-	sort(edges.begin(), edges.end());
+	sort(edges, edges + m, [](array<int, 3> a, array<int, 3> b) {
+		return a[2] < b[2];
+	});
 
 	DSU d(n);
 	int c = n;
-	vector<vector<pair<int, int>>> graph(n);
-	for (tuple<int, int, int> t : edges) {
-		int w, u, v;
-		tie(w, u, v) = t;
+	range(i, 0, m) {
+		int w = edges[i][2], u = edges[i][0], v = edges[i][1];
 
 		if (d.unite(u, v)) {
 			graph[u].push_back({v, w});
@@ -99,17 +95,20 @@ int32_t main() {
 		h++;
 	}
 
-	vector<vector<int>> p(n, vector<int>(h, -1)), x(n, vector<int>(h));
-	vector<int> l(n, -1);
-	l[0] = 0;
-	dfs(graph, l, p, x, 0);
-	
 	range(i, 0, n) {
-		range(j, 1, h) {
-			if (p[i][j - 1] == -1) {
-				break;
+		fill(p[i], p[i] + h, -1);
+		fill(x[i], x[i] + h, -1);
+		l[i] = -1;
+	}
+	l[0] = 0;
+	dfs(0);
+
+	range(j, 1, h) {
+		range(i, 0, n) {
+			if (p[i][j - 1] != -1) {
+				p[i][j] = p[p[i][j - 1]][j - 1];
+				x[i][j] = max(x[i][j - 1], x[p[i][j - 1]][j - 1]);
 			}
-			x[i][j] = max(x[i][j - 1], x[p[i][j - 1]][j - 1]);
 		}
 	}
 
@@ -125,13 +124,20 @@ int32_t main() {
 		if (l[u] < l[v]) {
 			swap(u, v);
 		}
+		
+		if (p[u][0] == v) {
+			print(x[u][0]);
+			continue;
+		}
 
 		int res = 0;
+		int c = (1 << (h - 1));
 		for (int j = h - 1; j >= 0; j--) {
-			if (l[u] - (1 << j) >= l[v]) {
+			if (l[u] - c >= l[v]) {
 				res = max(res, x[u][j]);
 				u = p[u][j];
 			}
+			c >>= 1;
 		}
 
 		if (u == v) {
@@ -157,4 +163,3 @@ int32_t main() {
 		print(res);
 	}
 }
-
