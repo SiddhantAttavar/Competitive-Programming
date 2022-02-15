d77dac4d79b9fc12cbc1580e37e7e8c205bd5c3c
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-02-15 12:22:18 +0530

    UFDS: Mock INOI Uplsolving: 4A, 4B, 4C

--- ICO/UFDS 2022/Mock INOI Upsolving/4B_Lego_Towers.cpp
+++ ICO/UFDS 2022/Mock INOI Upsolving/4B_Lego_Towers.cpp
@@ -1,17 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T> inline void input(T& inVar) {cin >> inVar;}
-template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
-template<typename T> inline void print(T outVar) {cout << outVar << endl;}
-template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
-#define range(it, start, end) for (int it = start; it < end; it++)
-#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
-#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << endl
+//#define endl '\n'
+template<typename T> inline void print(T x) {cout << x << endl;}
+template<typename T, typename... S> inline void print(T x, S... y) {cout << x << ' '; print(y ...);}
+template<typename T> inline void input(T& x) {cin >> x;}
+template<typename T, typename... S> inline void input(T& x, S&... y) {cin >> x; input(y ...);}
+#define range(i, j, n) for (int i = j; i < n; i++)
+#define arrPut(x) for (auto &i : x) {cin >> i;}
+#define arrPrint(x) for (auto i : x) {cout << i << ' ';} cout << endl;
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define int long long
 
 int32_t main() {
-	setup(); int tc; input(tc); while (tc--) {
+	setup();
+
+	const int MOD = 1e9 + 7;
+
+	int n;
+	input(n);
+	vector<int> a(n);
+	arrPut(a);
+
+	vector<int> l(n), r(n);
+	stack<int> s;
+	range(i, 0, n) {
+		while (!s.empty() and a[i] <= a[s.top()]) {
+			s.pop();
+		}
 		
+		if (s.empty()) {
+			l[i] = -1;
+		}
+		else {
+			l[i] = s.top();
+		}
+		s.push(i);
 	}
+
+	while (!s.empty()) {
+		s.pop();
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		while (!s.empty() and a[i] <= a[s.top()]) {
+			s.pop();
+		}
+		
+		if (s.empty()) {
+			r[i] = n;
+		}
+		else {
+			r[i] = s.top();
+		}
+		s.push(i);
+	}
+	// arrPrint(l);
+	// arrPrint(r);
+
+	vector<vector<int>> dp(n, vector<int>(n));
+	range(i, 0, n) {
+		range(j, 0, n) {
+			if (j > 0) {
+				dp[i][j] = dp[i][j - 1];
+			}
+			if (l[j] < i and i < r[j]) {
+				if (i == 0) {
+					dp[i][j] = (dp[i][j] + 1) % MOD;
+				}
+				else {
+					dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+				}
+			}
+		}
+	}
+	// range(i, 0, n) {
+	// 	arrPrint(dp[i]);
+	// }
+	print(dp[n - 1][n - 1]);
 }
