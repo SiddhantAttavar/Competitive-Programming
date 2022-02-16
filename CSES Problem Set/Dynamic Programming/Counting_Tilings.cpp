9a90631aa743f3ed28354635299b04c07f1a21c4
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2022-02-16 12:37:21 +0530

    CSES: Counting Tilings

--- CSES Problem Set/Dynamic Programming/Counting_Tilings.cpp
+++ CSES Problem Set/Dynamic Programming/Counting_Tilings.cpp
@@ -1,74 +1,53 @@
 #include <bits/stdc++.h>
-#define range(it, start, end) for (int it = start; it < end; it++)
-#define input(x) cin >> x
-#define print(x) cout << x << endl
-#define arrPut(var) for (auto &i : var) {cin >> i;}
-#define arrPrint(var) for (auto outVar : var) {cout << outVar << " ";} cout << endl
-#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 using namespace std;
-typedef long long ll;
-const int MOD = 1e9 + 7;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
 
-ll fact[11] = {1};
-ll dp[1001][11];
-
-ll comb(int x, int y) {
-	return (fact[x + y] / (fact[x] * fact[y])) % MOD;
-}
-
-int main() {
+int32_t main() {
 	setup();
 	
+	const int MOD = 1e9 + 7;
 	int n, m;
-	input(n);
-	input(m);
+	input(n, m);
 
-	if ((m * n) % 2 == 1) {
-		print(0);
-		return 0;
-	}
-
-	range(i, 1, n + 1) {
-		fact[i] = i * fact[i - 1];
-	}
-
-	//For i = 0, dp[0][0] = 1, rest all are 0
-	dp[0][0] = 1;
-
-	//i represents the width of the rectangle
+	vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m + 1, vector<int>(1 << (n + 1))));
+	dp[n][0][0] = 1;
 	range(i, 1, m + 1) {
-		//j represents the number of squares cut
-		range(j, 0, n) {
-			int left = n - j;
+		range(j, 0, 1 << n) {
+			dp[0][i][j << 1] = dp[n][i - 1][j];
+		}
 
-			if ((i * n + j) % 2 == 1) {
-				continue;
-			}
+		range(j, 1, n + 1) {
+			int x = 1 << (j - 1);
+			int y = 1 << j;
 
-			//Iterate through number of horizontal blocks
-			for (int k = left % 2; k <= left; k += 2) {
-				//The number of vertical blocks is (left - k) / 2
-				int l = (left - k) / 2;
+			range(k, 0, 1 << (n + 1)) {
+				dp[j - 1][i][k] %= MOD;
 
-				//The number of combinations of vertical and horizontal blocks
-				//is comb(k, l)
-				//The number of squares cut from previous level is k
-				//So the remaining ways are dp[i - 1][k]
-				if (dp[i - 1][k] > 0) {
-					dp[i][j] = (dp[i][j] + comb(k, l) * dp[i - 1][k]) % MOD;
+				if ((k & x) and (k & y)) {
+					continue;
+				}
+
+				if (k & x) {
+					dp[j][i][k ^ x] += dp[j - 1][i][k];
+				}
+				else if (k & y) {
+					dp[j][i][k ^ y] += dp[j - 1][i][k];
+				}
+				else {
+					dp[j][i][k ^ x] += dp[j - 1][i][k];
+                    dp[j][i][k ^ y] += dp[j - 1][i][k];
 				}
 			}
 		}
-		dp[i][n] = dp[i - 1][0];
 	}
-
-	range(i, 0, m + 1) {
-		range(j, 0, n + 1) {
-			cout << dp[i][j] << " ";
-		}
-		print("");
-	}
-
-	//The answer is dp[m][0]
-	print(dp[m][0]);
+	
+	print(dp[n][m][0] % MOD);
 }
