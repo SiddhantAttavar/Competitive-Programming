a9813a48cb4ce1d5e9c91416adc8da0f10f29fc7
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-09-11 13:23:58 +0530

    Codeforces: problemset

---
+++ Other Problems/Codeforces Problemset/A. Next Round.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+
+int32_t main() {
+	setup();
+
+	int n, k;
+	input(n, k);
+
+	vector<int> v(n);
+	arrPut(v);
+	reverse(v.begin(), v.end());
+
+	print(v.end() - lower_bound(v.begin(), v.end(), max(1ll, v[n - k])));
+}
