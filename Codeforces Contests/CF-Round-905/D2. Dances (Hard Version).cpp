eb39c8194a4e2dddeb5f7a861cbc8d272be71c38
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-10-22 19:21:50 +0530

    CF-Round-905 Problem D2

--- Codeforces Contests/CF-Round-905/D2. Dances (Hard Version).cpp
+++ Codeforces Contests/CF-Round-905/D2. Dances (Hard Version).cpp
@@ -10,6 +10,27 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define int long long
 
+int solve(int n, vector<int> &a, vector<int> &b, int x) {
+	int k = lower_bound(a.begin(), a.end(), x) - a.begin();
+	a.insert(a.begin() + k, x);
+	// arrPrint(a);
+	// arrPrint(b);
+	int j = 0;
+	int res = 0;
+	range(i, 0, n) {
+		while (j < n and b[j] <= a[i]) {
+			j++;
+		}
+		if (j < n) {
+			res++;
+			j++;
+		}
+	}
+	// print('d', res);
+	a.erase(a.begin() + k);
+	return n - res;
+}
+
 int32_t main() {
 	setup(); int tc; input(tc); while (tc--) {
 		int n, m;
@@ -17,28 +38,27 @@ int32_t main() {
 
 		vector<int> a(n - 1);
 		arrPut(a);
-		a.insert(a.begin(), 1);
 
 		vector<int> b(n);
 		arrPut(b);
 
 		sort(a.begin(), a.end());
 		sort(b.begin(), b.end());
-		// arrPrint(a);
-		// arrPrint(b);
 
-		int j = 0;
-		int res = 0;
-		range(i, 0, n) {
-			while (j < n and b[j] <= a[i]) {
-				j++;
+		int x = solve(n, a, b, 1);
+		// print(x);
+		int l = 1, r = m, res = -1;
+		while (l <= r) {
+			int k = (l + r) / 2;
+			if (solve(n, a, b, k) == x) {
+				l = k + 1;
+				res = k;
 			}
-			if (j < n) {
-				res++;
-				j++;
+			else {
+				r = k - 1;
 			}
 		}
-		print(n - res);
+		print(x * res + (x + 1) * (m - res));
 	}
 }
 
