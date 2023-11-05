1299012b44c68142a58399809a2ea50efb5c092f
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-11-05 20:57:56 +0530

    cses sort and search

--- CSES Problem Set/Sorting and Searching/Reading_Books.cpp
+++ CSES Problem Set/Sorting and Searching/Reading_Books.cpp
@@ -1,35 +1,30 @@
 #include <bits/stdc++.h>
-#define range(it, start, end) for (int it = start; it < end; it++)
-#define input(x) cin >> x
-#define print(x) cout << x << endl
-#define arrPut(var) for (auto &i : var) {cin >> i;}
-#define arrPrint(var) for (auto outVar : var) {cout << outVar << " ";} cout << endl
-#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 using namespace std;
-typedef long long ll;
-const int MOD = 1e9 + 7;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
 
-int main() {
+int32_t main() {
 	setup();
-	
-	int n; input(n);
-	int t[n]; arrPut(t);
 
-	//K starts from the first book, but J starts from the last book
-	//Conflict arises if time taken to read first n - 1 books is less
-	//than time to read to read last book
+	int n;
+	input(n);
 
-	ll sum = 0;
-	for (int i : t) {
-		sum += i;
+	vector<int> a(n);
+	arrPut(a);
+
+	int m = *max_element(a.begin(), a.end());
+
+	int s = 0;
+	for (int i : a) {
+		s += i;
 	}
 
-	int max = *max_element(t, t + n);
-
-	if (sum - max < max) {
-		print(2 * max);
-	}
-	else {
-		print(sum);
-	}
+	print(max(2 * m, s));
 }
