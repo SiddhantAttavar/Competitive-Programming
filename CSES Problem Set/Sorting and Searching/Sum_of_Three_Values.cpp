c35cc13f03b00ae7ea1097cdb28ad0fe30b58d5b
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-11-22 09:09:00 +0530

    cses flight discount

--- CSES Problem Set/Sorting and Searching/Sum_of_Three_Values.cpp
+++ CSES Problem Set/Sorting and Searching/Sum_of_Three_Values.cpp
@@ -1,45 +1,34 @@
 #include <bits/stdc++.h>
-#define range(it, start, end) for (int it = start; it < end; it++)
-#define input(x) cin >> x
-#define print(x) cout << x << endl
-#define arrPut(var) for (auto &i : var) {cin >> i;}
-#define arrPrint(var) for (auto outVar : var) {cout << outVar << " ";} cout << endl
-#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 using namespace std;
-typedef long long ll;
-const int MOD = 1e9 + 7;
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
 
-int main() {
+int32_t main() {
 	setup();
-	
+
 	int n, x;
-	input(n);
-	input(x);
+	input(n, x);
 
-	int a[n]; arrPut(a);
-	map<int, vector<int>> m;
-	range(i, 0, n) {
-		m[a[i]].push_back(i);
-	}
+	vector<int> a(n);
+	arrPut(a);
 
-	range(i, 0, n) {
-		range(j, 0, i) {
-			int left = x - a[i] - a[j];
-			if (m.find(left) != m.end()) {
-				int k = -1;
-				for (int l : m[left]) {
-					if (l != i && l != j) {
-						k = l;
-						break;
-					}
-				}
-				if (k != -1) {
-					print(i + 1 << " " << j + 1 << " " << k + 1);
-					return 0;
-				}
+	map<int, int> m;
+	m[a[0]] = 0;
+	range(i, 1, n) {
+		range(j, i + 1, n) {
+			if (m.count(x - a[i] - a[j])) {
+				print(m[x - a[i] - a[j]] + 1, i + 1, j + 1);
+				return 0;
 			}
 		}
+		m[a[i]] = i;
 	}
-
 	print("IMPOSSIBLE");
 }
