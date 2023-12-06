84d41bff011ebbc9bee0cad957d59a2887d79714
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-06 19:37:06 +0530

    AOC D6

--- Other Contests/AdventOfCode/2023/Day6/Day6Q1.py
+++ Other Contests/AdventOfCode/2023/Day6/Day6Q1.py
@@ -1,9 +1,32 @@
+from math import ceil, sqrt
 from aocd import get_data, submit
 data = get_data(day = 6, year = 2023).splitlines()
-# data = ''''''.splitlines()
+# data = '''Time:      7  15   30
+# Distance:  9  40  200'''.splitlines()
 
-res = 0
+res = 1
+t = list(map(int, data[0].split()[1:]))
+d = list(map(int, data[1].split()[1:]))
+for i, j in zip(t, d):
+	l = 1
+	r = i // 2
+	x = -1
+	while l <= r:
+		m = (l + r) // 2
+		if m * (i - m) > j:
+			x = m
+			r = m - 1
+		else:
+			l = m + 1
 
+	# print(x)
+
+	if x == -1:
+		res = 0
+		break
+
+	res *= i - 2 * x + 1
+	# print(res)
 
 print(res)
-# submit(res, part = a, day = 6, year = 2023)
+# submit(res, part = 'a', day = 6, year = 2023)
