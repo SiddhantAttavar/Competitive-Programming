fcd11801c709ee52e35e14be11f67b04c7d795d9
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-11 23:28:21 +0530

    AOC D11

--- Other Contests/AdventOfCode/2023/Day11/Day11Q1.py
+++ Other Contests/AdventOfCode/2023/Day11/Day11Q1.py
@@ -1,9 +1,54 @@
 from aocd import get_data, submit
 data = get_data(day = 11, year = 2023).splitlines()
-# data = ''''''.splitlines()
+# data = '''...#......
+# .......#..
+# #.........
+# ..........
+# ......#...
+# .#........
+# .........#
+# ..........
+# .......#..
+# #...#.....'''.splitlines()
 
 res = 0
+h = []
+n = len(data)
+m = len(data[0])
+for i in range(n):
+	if data[i] == '.' * m:
+		h.append(i)
+for i in reversed(h):
+	data.insert(i, '.' * m)
+n += len(h)
+v = []
+for j in range(m):
+	for i in range(n):
+		if data[i][j] != '.':
+			break
+	else:
+		v.append(j)
+for j in reversed(v):
+	for i in range(n):
+		# print(j, ''.join(data[i][:j]) + '.' + ''.join(data[i][j:]))
+		data[i] = ''.join(data[i][:j]) + '.' + ''.join(data[i][j:])
 
+# print(*data, sep = '\n')
+# print(h, v)
+m += len(v)
+# print(len(data), len(data[0]))
 
+l = []
+for i in range(n):
+	for j in range(m):
+		if data[i][j] == '#':
+			l.append((i, j))
+
+res = 0
+for x, y in l:
+	for a, b in l:
+		res += abs(a - x) + abs(b - y)
+
+res //= 2
 print(res)
-# submit(res, part = a, day = 11, year = 2023)
+# submit(res, part = 'a', day = 11, year = 2023)
--- Other Contests/AdventOfCode/2023/Day11/Day11Q2.py
+++ Other Contests/AdventOfCode/2023/Day11/Day11Q2.py
@@ -1,9 +1,52 @@
 from aocd import get_data, submit
 data = get_data(day = 11, year = 2023).splitlines()
-# data = ''''''.splitlines()
+# data = '''...#......
+# .......#..
+# #.........
+# ..........
+# ......#...
+# .#........
+# .........#
+# ..........
+# .......#..
+# #...#.....'''.splitlines()
 
 res = 0
+h = []
+v = []
+n = len(data)
+m = len(data[0])
+for i in range(n):
+	if data[i] == '.' * m:
+		h.append(i)
+for j in range(m):
+	for i in range(n):
+		if data[i][j] != '.':
+			break
+	else:
+		v.append(j)
 
+# print(*data, sep = '\n')
+# print(h, v)
+# print(len(data), len(data[0]))
 
+l = []
+for i in range(n):
+	for j in range(m):
+		if data[i][j] == '#':
+			l.append((i, j))
+
+res = 0
+for x, y in l:
+	for a, b in l:
+		p = 0
+		for i in h:
+			p += (a <= i and i < x) or (x <= i and i < a)
+		q = 0
+		for j in v:
+			q += (b <= j and j < y) or (y <= j and j < b)
+		res += abs(a - x) + abs(b - y) + 999999 * (p + q)
+
+res //= 2
 print(res)
-# submit(res, part = b, day = 11, year = 2023)
+# submit(res, part = 'b', day = 11, year = 2023)
