5461d27323c508e5ba566f6f0002616a09e7123f
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-14 13:49:43 +0530

    AOC D14 P2

--- Other Contests/AdventOfCode/2023/Day14/Day14Q2.py
+++ Other Contests/AdventOfCode/2023/Day14/Day14Q2.py
@@ -1,16 +1,16 @@
 import numpy as np
 from aocd import get_data, submit
 data = get_data(day = 14, year = 2023).splitlines()
-data = '''O....#....
-O.OO#....#
-.....##...
-OO.#O....O
-.O.....O#.
-O.#..O.#.#
-..O..#O..O
-.......O..
-#....###..
-#OO..#....'''.splitlines()
+# data = '''O....#....
+# O.OO#....#
+# .....##...
+# OO.#O....O
+# .O.....O#.
+# O.#..O.#.#
+# ..O..#O..O
+# .......O..
+# #....###..
+# #OO..#....'''.splitlines()
 
 def printMat(data):
 	for i in data:
@@ -38,34 +38,59 @@ def next_cycle(data):
 					y = i + 1 
 		data = np.rot90(data, k = 3)
 
-slow = np.copy(data)
-fast = np.copy(data)
-next_cycle(fast)
-while (slow != fast).any():
-	next_cycle(slow)
-	next_cycle(fast)
-	next_cycle(fast)
-	# printMat(slow)
-	# printMat(fast)
+t = 0
+time = {}
+states = []
+fin = ''
+
+def dfs(u, x):
+	global fin
+	k = '\n'.join([''.join(map(str, i)) for i in u])
+	# print(k)
 	# print()
+	if k in time:
+		cycle_size = x - time[k]
+		rem = (1000000000 - time[k]) % cycle_size
+		print(cycle_size, time[k], rem)
+		fin = states[time[k] + rem]
+		return
 
-x = 1000000000
-u = np.copy(data)
-while (u != slow).any():
+	time[k] = len(states)
+	states.append(k)
 	next_cycle(u)
-	x -= 1
+	dfs(u, x + 1)
 
-y = 1
-next_cycle(u)
-while (u != slow).any():
-	next_cycle(u)
-	y += 1
+dfs(data, 0)
+print(fin)
 
+# slow = np.copy(data)
+# fast = np.copy(data)
+# next_cycle(fast)
+# while (slow != fast).any():
+# 	next_cycle(slow)
+# 	next_cycle(fast)
+# 	next_cycle(fast)
+# 	# printMat(slow)
+# 	# printMat(fast)
+# 	# print()
+#
+# x = 1000000000
+# u = np.copy(data)
+# while (u != slow).any():
+# 	next_cycle(u)
+# 	x -= 1
+#
+# y = 1
+# next_cycle(u)
+# while (u != slow).any():
+# 	next_cycle(u)
+# 	y += 1
+#
 # print(x, y)
-
-for i in range(x % y):
-	next_cycle(u)
-printMat(u);
+#
+# for i in range(x % y):
+# 	next_cycle(u)
+# printMat(u)
 
 # res = 0
 # old_data = np.zeros(data.shape)
@@ -84,13 +109,16 @@ printMat(u);
 # data = np.rot90(data, k = 3)
 # printMat(data)
 
-n = len(u)
-m = len(u[0])
+fin = fin.splitlines()
+n = len(fin)
+m = len(fin[0])
+print()
+print(n, m)
 res = 0
-for j in range(m):
-	for i in range(1, n):
-		if u[i][j] == 0:
+for i in range(n):
+	for j in range(m):
+		if fin[i][j] == '0':
 			res += n - i
 
 print(res)
-# submit(res, part = 'b', day = 14, year = 2023)
+submit(res, part = 'b', day = 14, year = 2023)
