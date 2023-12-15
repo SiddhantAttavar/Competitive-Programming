24bdb6e893fb0c529276012a185128771cac36c3
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-15 21:25:21 +0530

    Update generation script

--- Other Contests/AdventOfCode/script.sh
+++ Other Contests/AdventOfCode/script.sh
@@ -4,6 +4,6 @@ mkdir -p $year
 for day in $(seq 1 25); do
 	mkdir -p "$year/Day$day"
 	echo "Created $year/Day$day/Day${day}Q1.py"
-	sed "s/\$year/$year/g;s/\$day/$day/g;s/\$part/a/g" AOCTemplate.py > "$year/Day$day/Day${day}Q1.py"
-	sed "s/\$year/$year/g;s/\$day/$day/g;s/\$part/b/g" AOCTemplate.py > "$year/Day$day/Day${day}Q2.py"
+	sed "s/\$year/$year/g;s/\$day/$day/g;s/\$part/'a'/g" AOCTemplate.py > "$year/Day$day/Day${day}Q1.py"
+	sed "s/\$year/$year/g;s/\$day/$day/g;s/\$part/'b'/g" AOCTemplate.py > "$year/Day$day/Day${day}Q2.py"
 done
