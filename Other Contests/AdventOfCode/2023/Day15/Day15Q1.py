f337ae08d0822efc92880d931dbd1d8e24dc60b4
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-15 21:24:48 +0530

    AOC D15

--- Other Contests/AdventOfCode/2023/Day15/Day15Q1.py
+++ Other Contests/AdventOfCode/2023/Day15/Day15Q1.py
@@ -1,9 +1,17 @@
 from aocd import get_data, submit
 data = get_data(day = 15, year = 2023).splitlines()
-# data = ''''''.splitlines()
+# data = '''rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'''.splitlines()
+
+def hash(s):
+	res = 0
+	for c in s:
+		res = (17 * (res + ord(c))) % 256
+	return res
 
 res = 0
 
+for r in data[0].split(','):
+	res += hash(r)
 
 print(res)
-# submit(res, part = a, day = 15, year = 2023)
+submit(res, part = 'a', day = 15, year = 2023)
