a9a531cf05ad671861e01ce587daa7ae9664a61a
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2023-12-16 18:54:22 +0530

    AOC D16

--- Other Contests/AdventOfCode/2023/Day16/Day16Q1.py
+++ Other Contests/AdventOfCode/2023/Day16/Day16Q1.py
@@ -1,9 +1,52 @@
+from queue import Queue
 from aocd import get_data, submit
 data = get_data(day = 16, year = 2023).splitlines()
-# data = ''''''.splitlines()
+data = r'''.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....'''.splitlines()
 
 res = 0
 
+# print(data)
+
+n = len(data)
+m = len(data[0])
+d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+l = [
+	{'.': [0], '|': [2, 3], '-': [0], '\\': [2], '/': [3]},
+	{'.': [1], '|': [2, 3], '-': [1], '\\': [3], '/': [2]},
+	{'.': [2], '|': [2], '-': [0, 1], '\\': [0], '/': [1]},
+	{'.': [3], '|': [3], '-': [0, 1], '\\': [1], '/': [0]},
+]
+
+q = Queue()
+q.put(((0, 0), 0))
+v = [[[False] * 4 for _ in range(m)] for _ in range(n)]
+v[0][0][0] = True
+
+while not q.empty():
+	(x, y), k = q.get()
+	# print(x, y, k)
+
+	for i in l[k][data[x][y]]:
+		a, b = x + d[i][0], y + d[i][1]
+		if a >= 0 and b >= 0 and a < n and b < m and not v[a][b][i]:
+			v[a][b][i] = True
+			q.put(((a, b), i))
+
+for i in range(n):
+	for j in range(m):
+		for k in range(4):
+			if v[i][j][k]:
+				res += 1
+				break
 
 print(res)
-# submit(res, part = a, day = 16, year = 2023)
+# submit(res, part = 'a', day = 16, year = 2023)
--- Other Contests/AdventOfCode/2023/Day16/Day16Q2.py
+++ Other Contests/AdventOfCode/2023/Day16/Day16Q2.py
@@ -1,9 +1,64 @@
+from queue import Queue
 from aocd import get_data, submit
 data = get_data(day = 16, year = 2023).splitlines()
-# data = ''''''.splitlines()
+# data = r'''.|...\....
+# |.-.\.....
+# .....|-...
+# ........|.
+# ..........
+# .........\
+# ..../.\\..
+# .-.-/..|..
+# .|....-|.\
+# ..//.|....'''.splitlines()
 
 res = 0
 
+# print(data)
+
+n = len(data)
+m = len(data[0])
+d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+l = [
+	{'.': [0], '|': [2, 3], '-': [0], '\\': [2], '/': [3]},
+	{'.': [1], '|': [2, 3], '-': [1], '\\': [3], '/': [2]},
+	{'.': [2], '|': [2], '-': [0, 1], '\\': [0], '/': [1]},
+	{'.': [3], '|': [3], '-': [0, 1], '\\': [1], '/': [0]},
+]
+
+s = []
+for i in range(n):
+	s.append(((i, 0), 0))
+	s.append(((i, m - 1), 1))
+for i in range(m):
+	s.append(((0, i), 2))
+	s.append(((n - 1, i), 3))
+
+for (e, f), t in s:
+	q = Queue()
+	q.put(((e, f), t))
+	v = [[[False] * 4 for _ in range(m)] for _ in range(n)]
+	v[e][f][t] = True
+
+	while not q.empty():
+		(x, y), k = q.get()
+		# print(x, y, k)
+
+		for i in l[k][data[x][y]]:
+			a, b = x + d[i][0], y + d[i][1]
+			if a >= 0 and b >= 0 and a < n and b < m and not v[a][b][i]:
+				v[a][b][i] = True
+				q.put(((a, b), i))
+
+	curr = 0
+	for i in range(n):
+		for j in range(m):
+			for k in range(4):
+				if v[i][j][k]:
+					curr += 1
+					break
+	# print(e, f, t, curr)
+	res = max(res, curr)
 
 print(res)
-# submit(res, part = b, day = 16, year = 2023)
+# submit(res, part = 'b', day = 16, year = 2023)
