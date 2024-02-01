13dae1c4b2de4be15e454ca8d098e5fe7d5b8209
Siddhant Attavar <siddhantacounts@gmail.com>
2024-02-01 12:24:27 +0530

    xorred

--- CodeChef Contests/Starters-119/Xorred.cpp
+++ CodeChef Contests/Starters-119/Xorred.cpp
@@ -22,53 +22,5 @@ int32_t main() {
 		vector<int> a(n);
 		arrPut(a);
 
-		map<int, vector<int>> m;
-		m[0].push_back(-1);
-
-		vector<int> b(30, -1);
-		int x = 0;
-		int res = 0;
-		range(i, 0, n) {
-			x ^= a[i];
-
-			range(j, 0, 30) {
-				if (!((1 << j) & a[i])) {
-					b[j] = i;
-				}
-			}
-
-			vector<pair<int, int>> c(30);
-			range(i, 0, 30) {
-				c[i] = {b[i], i};
-			}
-			sort(c.begin(), c.end());
-			reverse(c.begin(), c.end());
-
-			int y = (1 << 30) - 1;
-			vector<pair<int, int>> v;
-			range(j, 0, 30) {
-				y ^= 1 << c[j].second;
-				if (j > 0 and c[j].first == c[j - 1].first) {
-					v.back().first = y;
-				}
-				else {
-					v.push_back({y, c[j].first});
-				}
-			}
-
-			if (v.back().second != -1) {
-				v.push_back({-1, -1});
-			}
-			range(j, 0, (int) v.size() - 1) {
-				int r = v[j].second;
-				int l = v[j + 1].second + 1;
-				int z = x ^ v[j].first;
-				res += m[z].end() - lower_bound(m[z].begin(), m[z].end(), l - 1);
-			}
-
-			m[x].push_back(i);
-		}
-
-		print(res);
 	}
 }
