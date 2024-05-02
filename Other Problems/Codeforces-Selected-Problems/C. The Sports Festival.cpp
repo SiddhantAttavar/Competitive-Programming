ca415d7e83134a96585c08a053059d5ff83c5ed3
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-05-02 13:56:21 +0530

    Sports festival

---
+++ Other Problems/Codeforces-Selected-Problems/C. The Sports Festival.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define int long long
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+const int MOD = (int) 1e9 + 7;
+
+int32_t main() {
+	setup();
+
+	int n;
+	input(n);
+
+	vector<int> s(n);
+	arrPut(s);
+	sort(s.begin(), s.end());
+
+	vector<vector<int>> dp(n, vector<int>(n, 0));
+	range(l, 2, n + 1) {
+		range(i, 0, n - l + 1) {
+			int j = i + l - 1;
+			dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + s[j] - s[i];
+		}
+	}
+	print(dp[0][n - 1]);
+}
