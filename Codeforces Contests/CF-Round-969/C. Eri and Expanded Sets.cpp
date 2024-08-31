c0181e4fff5581caa46c11af3995da3e5ea1a09a
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-08-31 12:07:22 +0530

    CF 969 C

--- Codeforces Contests/CF-Round-969/C. Eri and Expanded Sets.cpp
+++ Codeforces Contests/CF-Round-969/C. Eri and Expanded Sets.cpp
@@ -15,6 +15,27 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7;
 
+template<class T> struct SegTree { // cmb(ID,b) = b
+	T ID; T (*cmb)(T a, T b);
+	int n; vector<T> seg;
+	SegTree(int _n, T id, T _cmb(T, T)) {
+		ID = id; cmb = _cmb;
+		for (n = 1; n < _n; ) n *= 2; 
+		seg.assign(2*n,ID); 
+	}
+	void pull(int p) { seg[p] = cmb(seg[2*p],seg[2*p+1]); }
+	void upd(int p, T val) { // set val at position p
+		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
+	T query(int l, int r) {	// zero-indexed, inclusive
+		T ra = ID, rb = ID;
+		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+			if (l&1) ra = cmb(ra,seg[l++]);
+			if (r&1) rb = cmb(seg[--r],rb);
+		}
+		return cmb(ra,rb);
+	}
+};
+
 int32_t main() {
 	setup(); int tc; input(tc); while (tc--) {
 		int n;
@@ -23,11 +44,37 @@ int32_t main() {
 		vector<int> a(n);
 		arrPut(a);
 
-		vector<int> dp(n);
-		int i = 0, l = a[0], r = a[0];
-		int res = 1;
-		range(j, 1, n) {
+		SegTree<int> s(n - 1, 0, [](int x, int y) {return __gcd(x, y);});
+		range(i, 0, n - 1) {
+			s.upd(i, abs(a[i + 1] - a[i]));
+		}
 
+		int res = 1;
+		vector<int> dp(n, 1);
+		for (int i = n - 2; i >= 0; i--) {
+			if (a[i] == a[i + 1]) {
+				dp[i] = dp[i + 1] + 1;
+			}
+			res += dp[i];
+		}
+
+		range(i, 0, n - 1) {
+			int l = i, r = n - 2, c = n - 1;
+			while (l <= r) {
+				int m = (l + r) / 2;
+				int x = s.query(i, m);
+				while (x and (x & 1) == 0) {
+					x >>= 1;
+				}
+				if (x == 1) {
+					c = m;
+					r = m - 1;
+				}
+				else {
+					l = m + 1;
+				}
+			}
+			res += n - 1 - c;
 		}
 		print(res);
 	}
