5a38e7bf73b86307df729dd79385c59e7dc413a3
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-09-05 17:39:34 +0530

    G2

--- Codeforces Contests/CF-Round-971/G2. Yunli's Subarray Queries (hard version).cpp
+++ Codeforces Contests/CF-Round-971/G2. Yunli's Subarray Queries (hard version).cpp
@@ -15,7 +15,126 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7;
 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using db = long double; // or double if tight TL
+using str = string;
+
+using pi = pair<int,int>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template<class T
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vpi = V<pi>;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rsz resize
+#define pb push_back
+#define ft front()
+#define bk back()
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+int seg[1<<21], lazy[1<<21]; 
+tcT, int SZ> struct LazySeg {
+	LazySeg() { F0R(i,2*SZ) {seg[i] = 0; lazy[i] = -1;} }
+	void push(int ind, int L, int R) { /// modify values for current node
+		if (lazy[ind] == -1) return;
+		seg[ind] = (R-L+1)*lazy[ind]; // dependent on operation
+		if (L != R) F0R(i,2) lazy[2*ind+i] = lazy[ind]; /// prop to children
+		lazy[ind] = -1; 
+	} // recalc values for current node
+	void pull(int ind){seg[ind]=seg[2*ind]+seg[2*ind+1];}
+	void build() { ROF(i,1,SZ) pull(i); }
+	void upd(int lo,int hi,T inc,int ind=1,int L=0, int R=SZ-1) {
+		push(ind,L,R); if (hi < L || R < lo) return;
+		if (lo <= L && R <= hi) { 
+			lazy[ind] = inc; push(ind,L,R); return; }
+		int M = (L+R)/2; upd(lo,hi,inc,2*ind,L,M); 
+		upd(lo,hi,inc,2*ind+1,M+1,R); pull(ind);
+	}
+	T query(int lo, int hi, int ind=1, int L=0, int R=SZ-1) {
+		push(ind,L,R); if (lo > R || L > hi) return 0;
+		if (lo <= L && R <= hi) return seg[ind];
+		int M = (L+R)/2; return (query(lo,hi,2*ind,L,M)+
+			query(lo,hi,2*ind+1,M+1,R));
+	}
+};
+
+/* struct LazySeg {
+	vector<int> a, b;
+	
+	LazySeg(int n) {
+		a.resize(4 * n, 0);
+		b.resize(4 * n, -1);
+	}
+
+	void push(int i, int l, int r) {
+		if (b[i] == -1) {
+			return;
+		}
+
+		a[i] = (r - l + 1) * b[i];
+		if (l != r) {
+			b[2 * i + 1] = b[i];
+			b[2 * i + 2] = b[i];
+		}
+		b[i] = -1;
+	}
+
+	void pull(int i) {
+		a[i] = a[2 * i + 1] + a[2 * i + 2];
+	}
+
+	void upd(int i, int x, int l, int r, int s, int e) {
+		push(i, l, r);
+		if (r < s or l > e) {
+			return;
+		}
+
+		if (l <= s and e <= r) {
+			b[i] = x;
+			push(i, l, r);
+			return;
+		}
+
+		int m = (s + e) / 2;
+		upd(2 * i + 1, x, l, r, s, m);
+		upd(2 * i + 2, x, l, r, m + 1, e);
+		pull(i);
+	}
+
+	int query(int i, int l, int r, int s, int e) {
+		push(i, l, r);
+		if (r < s or l > e) {
+			return 0;
+		}
+
+		if (l <= s and e <= r) {
+			return a[i];
+		}
+
+		int m = (s + e) / 2;
+		return query(2 * i + 1, l, r, s, m) + query(2 * i + 2, l, r, m + 1, e);
+	}
+} */;
+
 int32_t main() {
+	LazySeg<int, 1 << 18> t;
 	setup(); int tc; input(tc); while (tc--) {
 		int n, k, q;
 		input(n, k, q);
@@ -36,8 +155,8 @@ int32_t main() {
 			s.insert(p.second);
 		}
 
-		vector<int> res(n);
-		res[k - 1] = k - *s.rbegin();
+		vector<int> z(n);
+		z[k - 1] = k - *s.rbegin();
 		range(i, k, n) {
 			s.erase(s.find(m[a[i - k]]));
 			m[a[i - k]]--;
@@ -49,17 +168,46 @@ int32_t main() {
 			m[a[i]]++;
 			s.insert(m[a[i]]);
 
-			res[i] = k - *s.rbegin();
+			z[i] = k - *s.rbegin();
 		}
 
-		while (q--) {
+		vector<pair<pair<int, int>, int>> v(q);
+		range(i, 0, q) {
+			input(v[i].first.first, v[i].first.second);
+			v[i].second = i;
+		}
+		sort(v.begin(), v.end());
+		reverse(v.begin(), v.end());
+
+		stack<int> st;
+		st.push(n);
+
+		int i = n;
+		vector<int> res(q);
+		for (pair<pair<int, int>, int> p : v) {
 			int l, r;
-			input(l, r);
+			tie(l, r) = p.first;
 
 			l--;
 			r--;
+			l += k - 1;
 
-			print(res[r - 1]);
+			while (i > l) {
+				i--;
+
+				while (st.top() != n and z[i] < z[st.top()]) {
+					st.pop();
+				}
+
+				t.upd(i, st.top() - 1, z[i]);
+				st.push(i);
+			}
+
+			res[p.second] = t.query(l, r);
+		}
+
+		for (int i : res) {
+			print(i);
 		}
 	}
 }
