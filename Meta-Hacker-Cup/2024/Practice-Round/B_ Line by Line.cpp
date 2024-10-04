bf660f14e665a8203623253796614843d7fe1b6d
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-10-04 18:15:24 +0530

    meta hackercup practice round

---
+++ Meta-Hacker-Cup/2024/Practice-Round/B_ Line by Line.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds; 
+template<typename T> inline void input(T& inVar) {cin >> inVar;}
+template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin >> inVar; input(args ...);}
+template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
+template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
+#define int long long
+#define range(it, start, end) for (auto it = start; it < end; it++)
+#define arrPut(var) for (auto &inVar : var) {cin >> inVar;}
+#define arrPrint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+const int MOD = (int) 1e9 + 7; //998244353
+
+const double e = 1e-9;
+
+double check(int n, double x) {
+	return pow(x / 100, n);
+}
+
+int32_t main() {
+	cout << setprecision(20);
+	setup(); int tc; input(tc); range(t, 1, tc + 1) {
+		int n, p;
+		input(n, p);
+
+		cout << "Case #" << t << ": " << 100 * pow(p / 100.0, 1 - 1.0 / n) - p << endl;
+	}
+}
