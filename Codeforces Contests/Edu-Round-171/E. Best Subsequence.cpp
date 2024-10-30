8c3bc92e36453186592767864caee0e2eacf82bb
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-10-30 12:25:09 +0530

    Edu 171

--- Codeforces Contests/Edu-Round-171/E. Best Subsequence.cpp
+++ Codeforces Contests/Edu-Round-171/E. Best Subsequence.cpp
@@ -14,6 +14,90 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353
+							   //
+using ll = long long;
+using db = long double; // or double if tight TL
+using str = string;
+
+using pi = pair<int,int>;
+#define mp make_pair
+#define f first
+#define s second
+
+#define tcT template<class T
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>;
+using vi = V<int>;
+using vb = V<bool>;
+using vpi = V<pi>;
+
+#define sz(x) (int)((x).size())
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define rsz resize
+#define pb push_back
+#define ft front()
+#define bk back()
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep FOR
+#define each(a,x) for (auto& a: x)
+
+const db PI = acos((db)-1);
+mt19937 rng(0); // or mt19937_64
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+// int main() { cin.tie(0)->sync_with_stdio(0); }
+
+bool dfs(int a, int L, vector<vi>& g, vi& btoa, vi& A, vi& B) {
+	if (A[a] != L) return 0;
+	A[a] = -1;
+	for (int b : g[a]) if (B[b] == L + 1) {
+		B[b] = 0;
+		if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B))
+			return btoa[b] = a, 1;
+	}
+	return 0;
+}
+
+int hopcroftKarp(vector<vi>& g, vi& btoa) {
+	int res = 0;
+	vi A(g.size()), B(btoa.size()), cur, next;
+	for (;;) {
+		fill(all(A), 0);
+		fill(all(B), 0);
+		cur.clear();
+		for (int a : btoa) if(a != -1) A[a] = -1;
+		rep(a,0,sz(g)) if(A[a] == 0) cur.push_back(a);
+		for (int lay = 1;; lay++) {
+			bool islast = 0;
+			next.clear();
+			for (int a : cur) for (int b : g[a]) {
+				if (btoa[b] == -1) {
+					B[b] = lay;
+					islast = 1;
+				}
+				else if (btoa[b] != a && !B[b]) {
+					B[b] = lay;
+					next.push_back(btoa[b]);
+				}
+			}
+			if (islast) break;
+			if (next.empty()) return res;
+			for (int a : next) A[a] = lay;
+			cur.swap(next);
+		}
+		rep(a,0,sz(g))
+			res += dfs(a, 0, g, btoa, A, B);
+	}
+}
 
 int32_t main() {
 	setup(); int tc; input(tc); while (tc--) {
@@ -23,11 +107,16 @@ int32_t main() {
 		vector<int> a(n);
 		arrPut(a);
 
-		vector<int> v(60, 0);
-		for (int i : a) {
+		vector<vector<int>> graph(n);
+		range(i, 0, n) {
 			range(j, 0, 60) {
-				v[j] += (i & (1 << j)) > 0;
+				if (a[i] & (1ll << j)) {
+					graph[i].push_back(j);
+				}
 			}
 		}
+
+		vector<int> v(60, -1);
+		print(n - hopcroftKarp(graph, v));
 	}
 }
