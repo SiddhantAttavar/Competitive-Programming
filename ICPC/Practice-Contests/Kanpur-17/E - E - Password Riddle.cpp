1ee4e6b668d413b88e6d7b8fd46320ab54c187d8
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2024-12-23 11:09:30 +0530

    ICPC Kanpur 2017 E password riddle

--- ICPC/Practice-Contests/Kanpur-17/E - E - Password Riddle.cpp
+++ ICPC/Practice-Contests/Kanpur-17/E - E - Password Riddle.cpp
@@ -15,8 +15,156 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353
 
+map<string, int> m = {
+    {"zero", 0},
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
+};
+
+const int N = 600000;
+const int H = 20;
+bool dp[N + 1][H][H];
+
+bool match(vector<string> &a, pair<int, int> p) {
+    if (a.back() == "odd" and p.first % 2 == 0) {
+        return false;
+    }
+    else if (a.back() == "even" and p.first % 2 == 1) {
+        return false;
+    }
+    else if (m.count(a.back()) and p.first != m[a.back()]) {
+        return false;
+    }
+    a.pop_back();
+
+    int z = 1, o = 0, e = 0;
+    for (string s : a) {
+        if (s == "odd") {
+            o++;
+        }
+        else if (s == "even") {
+            e++;
+        }
+        else {
+            z *= m[s];
+        }
+    }
+
+    if (z == 0) {
+        return p.second == 0;
+    }
+
+    if (p.second % z) {
+        return false;
+    }
+    p.second /= z;
+
+    if (p.second == 0) {
+        return e > 0;
+    }
+
+    if (p.second == 1) {
+        return e == 0;
+    }
+
+    if (e >= H) {
+        return false;
+    }
+    return dp[p.second][min(o, H - 1)][e];
+}
+
 int32_t main() {
-    setup(); int tc; input(tc); while (tc--) {
-        
+    vector<int> v = {3, 5, 7, 9}, w = {2, 4, 6, 8};
+    dp[1][0][0] = true;
+    range(o, 0, H) {
+        range(e, 0, H) {
+            range(i, 1, N + 1) {
+                if (!dp[i][o][e]) {
+                    continue;
+                }
+
+                if (o < H - 1) {
+                    dp[i][o + 1][e] = true;
+                    for (int j : v) {
+                        if (i * j <= N) {
+                            dp[i * j][o + 1][e] = true;
+                        }
+                    }
+                }
+                if (e < H - 1) {
+                    for (int j : w) {
+                        if (i * j <= N) {
+                            dp[i * j][o][e + 1] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    setup(); int tc; input(tc); range(t, 1, tc + 1) {
+        cout << "Case " << t << ": ";
+        vector<vector<string>> a = {{}};
+        while (true) {
+            string s;
+            input(s);
+
+            string t = s;
+            if (t.back() == ',' or t.back() == '.') {
+                t.pop_back();
+            }
+            a.back().push_back(t);
+
+            if (s.back() == ',') {
+                a.push_back({});
+            }
+
+            if (s.back() == '.') {
+                break;
+            }
+        }
+
+        int n;
+        input(n);
+
+        string s;
+        input(s);
+
+        vector<pair<int, int>> v = {{s[0] - '0', 1}};
+        range(i, 1, n) {
+            if (s[i] == s[i - 1]) {
+                v.back().second++;
+            }
+            else {
+                v.push_back({s[i] - '0', 1});
+            }
+        }
+
+        int l = 0;
+        bool res = true;
+        for (pair<int, int> p : v) {
+            while (l < a.size() and !match(a[l], p)) {
+                l++;
+            }
+            if (l == a.size()) {
+                res = false;
+                break;
+            }
+            l++;
+        }
+
+        if (res) {
+            print("MAY BE");
+        }
+        else {
+            print("NO");
+        }
     }
 }
