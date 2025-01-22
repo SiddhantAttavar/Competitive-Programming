33d76105238540c3600b8c551f0b667762cb3eb1
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-01-22 16:43:18 +0530

    CF 999 E

--- Codeforces Contests/CF-Round-999/E. Kevin and And.cpp
+++ Codeforces Contests/CF-Round-999/E. Kevin and And.cpp
@@ -14,28 +14,6 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353
 
-#define vi vector<int>
-#define sz (int) size
-#define all(x) begin(x), end(x)
-#define rep range
-
-int knapsack(vi w, int t) {
-	int a = 0, b = 0, x;
-	while (b < sz(w) && a + w[b] <= t) a += w[b++];
-	if (b == sz(w)) return a;
-	int m = *max_element(all(w));
-	vi u, v(2*m, -1);
-	v[a+m-t] = b;
-	rep(i,b,sz(w)) {
-		u = v;
-		rep(x,0,m) v[x+w[i]] = max(v[x+w[i]], u[x]);
-		for (x = 2*m; --x > m;) rep(j, max(0,u[x]), v[x])
-			v[x-w[j]] = max(v[x-w[j]], j);
-	}
-	for (a = t; v[a+m-t] < 0; a--) ;
-	return a;
-}
-
 int32_t main() {
     setup(); int tc; input(tc); while (tc--) {
         int n, m, k;
