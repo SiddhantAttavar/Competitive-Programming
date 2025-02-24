a68f854d23b0da1d022b775b24cc8d6f23643cc3
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-02-24 18:59:46 +0530

    update template

--- Templates/Template.cpp
+++ Templates/Template.cpp
@@ -8,12 +8,12 @@ template<typename T, typename... S> inline void input(T& inVar, S&... args) {cin
 template<typename T> inline void print(T outVar) {cout << outVar << '\n';}
 template<typename T, typename... S> inline void print(T outVar, S... args) {cout << outVar << ' '; print(args ...);}
 #define int long long
-#define range(it, start, end) for (auto it = start; it < end; it++)
+#define rep(it, start, end) for (auto it = start; it < end; it++)
 #define arrput(var) for (auto &inVar : var) {cin >> inVar;}
 #define arrprint(var) for (auto outVar : var) {cout << outVar << ' ';} cout << '\n'
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
-const int MOD = (int) 1e9 + 7; //998244353
+const int MOD = (int) 1e9 + 7; //998244353;
 
 int32_t main() {
     setup(); int tc; input(tc); while (tc--) {
