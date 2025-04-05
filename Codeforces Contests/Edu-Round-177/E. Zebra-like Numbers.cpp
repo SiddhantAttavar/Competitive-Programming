142af138bd50f58ab777fe301a17fbf9ce6a310f
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-04-05 16:12:00 +0530

    Update kactl changes

--- Codeforces Contests/Edu-Round-177/E. Zebra-like Numbers.cpp
+++ Codeforces Contests/Edu-Round-177/E. Zebra-like Numbers.cpp
@@ -15,7 +15,37 @@ template<typename T, typename... S> inline void print(T outVar, S... args) {cout
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353;
 
+map<vector<int>, int> m;
+int solve(vector<int> &v, int i, int k) {
+	if (k < 0) {
+		return 0;
+	}
+	if (i == 0) {
+		return k <= v[i];
+	}
+
+	vector<int> z(v.begin(), v.end());
+	z.push_back(i);
+	z.push_back(k);
+	if (m.count(z)) {
+		return m[z];
+	}
+
+	int res = solve(v, i - 1, k - v[i]);
+	vector<int> w(30, 3);
+	rep(j, 0, v[i]) {
+		res += solve(w, i - 1, k - j);
+		res += (k - j) >= 4 and (k - j - 4) % 3 == 0 and (k - j - 4) / 3 <= i - 1;
+	}
+	return m[z] = res;
+}
+
 int32_t main() {
+	vector<int> a(30, 1);
+	rep(i, 1, 30) {
+		a[i] = 4 * a[i - 1] + 1;
+	}
+
 	setup(); int tc; input(tc); while (tc--) {
 		int l, r, k;
 		input(l, r, k);
@@ -25,10 +55,21 @@ int32_t main() {
 			continue;
 		}
 
-		vector<int> dp(k + 1);
-		rep(i, 0, k + 1) {
+		m.clear();
 
+		vector<int> v(30, 0);
+		for (int i = 29; i >= 0; i--) {
+			v[i] = r / a[i];
+			r %= a[i];
 		}
-		print(dp[k]);
+
+		vector<int> w(30, 0);
+		l--;
+		for (int i = 29; i >= 0; i--) {
+			w[i] = l / a[i];
+			l %= a[i];
+		}
+
+		print(solve(v, 29, k) - solve(w, 29, k));
 	}
 }
