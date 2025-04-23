a0c136aeadbf6794f9a7784ea3a2c69a6f9e35f5
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-04-23 10:07:40 +0530

    GoForGold April

--- Other Contests/GoForGold-Long-Contests/Apr-2025/A. Lonely Mountain.cpp
+++ Other Contests/GoForGold-Long-Contests/Apr-2025/A. Lonely Mountain.cpp
@@ -14,11 +14,93 @@ template<typename T, typename... S> inline void dbg(T x, S... args) {cerr << x <
 #define arrprint(l) for (auto i : l) {cout << i << ' ';} cout << '\n'
 #define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define int long long
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353;
 
 int32_t main() {
 	setup(); int tc; input(tc); while (tc--) {
+		int n;
+		input(n);
 
+		vector<int> a(n);
+		arrput(a);
+
+		map<int, int> m;
+		for (int i : a) {
+			m[i]++;
+		}
+
+		int c = 1;
+		map<int, int> t;
+		for (pair<int, int> p : m) {
+			t[p.first] = c++;
+		}
+		c--;
+
+		rep(i, 0, n) {
+			a[i] = t[a[i]];
+		}
+
+		vector<int> w(c + 1);
+		rep(i, 0, n) {
+			w[a[i]]++;
+		}
+		if (w[c] > 1 or *max_element(w.begin(), w.end()) > 2) {
+			print(-1);
+			continue;
+		}
+
+		vector<int> s(c + 1, 0);
+		rep(i, 0, c) {
+			s[i + 1] = s[i] + w[i + 1];
+		}
+
+		vector<int> p(c + 1, 1e18), q(c + 1, 1e18);
+		ordered_set o;
+		rep(i, 0, n) {
+			p[a[i]] = min(p[a[i]], (int) (o.size() - o.order_of_key(a[i] + 1)));
+			o.insert(a[i]);
+		}
+		o.clear();
+		for (int i = n - 1; i >= 0; i--) {
+			q[a[i]] = min(q[a[i]], (int) (o.size() - o.order_of_key(a[i] + 1)));
+			o.insert(a[i]);
+		}
+
+		int res = 0;
+		bool x = false, y = false;
+		rep(i, 1, c) {
+			if (w[i] == 2) {
+				res += p[i] + q[i];
+				x = true;
+				y = true;
+			}
+			else if (p[i] > q[i]) {
+				res += q[i];
+				y = true;
+			}
+			else {
+				res += p[i];
+				x = true;
+			}
+		}
+
+		if (!x) {
+			int z = 1e18;
+			rep(i, 1, c) {
+				z = min(z, p[i] - q[i]);
+			}
+			res += z;
+		}
+
+		if (!y) {
+			int z = 1e18;
+			rep(i, 1, c) {
+				z = min(z, q[i] - p[i]);
+			}
+			res += z;
+		}
+
+		print(res);
 	}
 }
--- Other Contests/GoForGold-Long-Contests/Apr-2025/C. How To Not Count 102.cpp
+++ Other Contests/GoForGold-Long-Contests/Apr-2025/C. How To Not Count 102.cpp
@@ -17,8 +17,118 @@ template<typename T, typename... S> inline void dbg(T x, S... args) {cerr << x <
 #define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
 const int MOD = (int) 1e9 + 7; //998244353;
 
-int32_t main() {
-	setup(); int tc; input(tc); while (tc--) {
-
+const __int128_t INF = (int) 1e18 + 1;
+map<array<int, 3>, int> memo;
+int calc(int i, int x, bool z, int n, int m, vector<int> &a, vector<int> &b, vector<__int128_t> &p) {
+	if (x < 1 or x > m) {
+		return 0;
 	}
+
+	if (z) {
+		__int128_t y = x;
+		if (i > 0) {
+			y -= b[i - 1] <= x;
+		}
+		if (i > 1) {
+			y -= b[i - 2] <= x;
+		}
+		// print((int) y, i, x, z, p[n - i - 1]);
+		return min(INF, y * p[n - i - 1]);
+	}
+
+	if (i == n) {
+		return 0;
+	}
+
+	if (x < a[i]) {
+		return 0;
+	}
+
+	array<int, 3> k = {i, x, z};
+	if (x == m and memo.count(k)) {
+		return memo[k];
+	}
+
+	__int128_t y = x - a[i];
+	if (i > 0) {
+		y -= a[i] < a[i - 1] and a[i - 1] <= x;
+	}
+	if (i > 1) {
+		y -= a[i] < a[i - 2] and a[i - 2] <= x;
+	}
+	y = min(INF, y * p[n - i - 1]);
+	if (i == 0 or a[i] != a[i - 1] and (i == 1 or a[i] != a[i - 2])) {
+		y += calc(i + 1, m, false, n, m, a, b, p);
+	}
+	y = min(INF, y);
+	if (x == m) {
+		memo[k] = y;
+	}
+	return y;
+}
+
+int32_t main() {
+	setup();
+
+	int n, m, k;
+	input(n, m, k);
+
+	vector<__int128_t> p(n, 1);
+	rep(i, 1, n - 1) {
+		p[i] = min(INF, p[i - 1] * (m - 2));
+	}
+	p[n - 1] = min(INF, p[n - 2] * (m - 1));
+
+	vector<int> a(n);
+	arrput(a);
+
+	vector<int> b(a.begin(), a.end());
+	if (calc(0, m, false, n, m, a, b, p) < k) {
+		print(-1);
+		return 0;
+	}
+
+	bool flag = false;
+	rep(i, 0, n) {
+		int l = flag ? 1 : a[i], r = m;
+		if (i > 0 and l == b[i - 1] or i > 1 and l == b[i - 2]) {
+			l++;
+		}
+		if (i > 0 and l == b[i - 1] or i > 1 and l == b[i - 2]) {
+			l++;
+		}
+		if (i > 0 and b[i] == b[i - 1] or i > 1 and b[i] == b[i - 2]) {
+			b[i]++;
+		}
+		if (i > 0 and b[i] == b[i - 1] or i > 1 and b[i] == b[i - 2]) {
+			b[i]++;
+		}
+		int o = l;
+		while (l <= r) {
+			int x = (l + r) / 2;
+			if (calc(i, x, flag, n, m, a, b, p) >= k) {
+				b[i] = x;
+				r = x - 1;
+			}
+			else {
+				l = x + 1;
+			}
+		}
+		k -= calc(i, b[i] - 1, flag, n, m, a, b, p);
+		if (i > 0 and b[i] == b[i - 1] or i > 1 and b[i] == b[i - 2]) {
+			b[i]--;
+		}
+		if (i > 0 and b[i] == b[i - 1] or i > 1 and b[i] == b[i - 2]) {
+			b[i]--;
+		}
+		if (b[i] > a[i]) {
+			flag = true;
+		}
+	}
+	arrprint(b);
+
+	// assert(*min_element(b.begin(), b.end()) > 0);
+	// assert(*max_element(b.begin(), b.end()) <= m);
+	// rep(i, 1, b.size()) assert(b[i] != b[i - 1]);
+	// rep(i, 2, b.size()) assert(b[i] != b[i - 2]);
 }
