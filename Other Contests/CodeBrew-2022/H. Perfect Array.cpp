e8ab3cd8e0b884444c7145565879323452f9fb94
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2025-12-10 03:59:56 +0530

    Codebrew: brobat contest

---
+++ Other Contests/CodeBrew-2022/H. Perfect Array.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+using namespace __gnu_pbds; 
+template<typename T> inline void input(T& x) {cin >> x;}
+template<typename T, typename... S> inline void input(T& x, S&... args) {cin >> x; input(args ...);}
+template<typename T> inline void print(T x) {cout << x << '\n';}
+template<typename T, typename... S> inline void print(T x, S... args) {cout << x << ' '; print(args ...);}
+#define debug(...) cout << #__VA_ARGS__ << ": "; print(__VA_ARGS__);
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define arrput(l) for (auto &i : l) {cin >> i;}
+#define arrprint(l) for (auto i : l) {cout << i << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+const int MOD = (int) 1e9 + 7; //998244353;
+
+int32_t main() {
+	setup();
+
+	int n;
+	input(n);
+
+	vector<int> a(n);
+	arrput(a);
+
+	vector<int> res;
+	while (!a.empty()) {
+		int u = -1;
+		for (int i = (int) a.size() - 1; i >= 0; i--) {
+			if (a[i] == i + 1) {
+				u = i;
+				break;
+			}
+		}
+		if (u == -1) {
+			print("NO");
+			return 0;
+		}
+		res.push_back(u + 1);
+		a.erase(a.begin() + u);
+	}
+	print("YES");
+	reverse(res.begin(), res.end());
+	arrprint(res);
+}
