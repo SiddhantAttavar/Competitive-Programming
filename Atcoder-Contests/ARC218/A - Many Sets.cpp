b86b14bbcb0378eb4c778856327fea38f8f931cb
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2026-05-03 20:26:56 +0530

    ARC218

---
+++ Atcoder-Contests/ARC218/A - Many Sets.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+using namespace __gnu_pbds; 
+template<typename T> inline void input(T& x) {cin >> x;}
+template<typename T, typename... S> inline void input(T& x, S&... args) {cin >> x; input(args ...);}
+template<typename T> inline void print(T x) {cout << x << '\n';}
+template<typename T, typename... S> inline void print(T x, S... args) {cout << x << ' '; print(args ...);}
+#define debug(...) cout << #__VA_ARGS__ << ": "; print(__VA_ARGS__);
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define arrput(l) for (auto &i : l) {cin >> i;}
+#define arrprint(l) for (auto i : l) {cout << i << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int) (x.size()))
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+typedef vector<int> vi; typedef pair<int, int> pii;
+const int MOD = 998244353;
+
+int mpow(int a, int b) {
+	int res = 1;
+	while (b) {
+		if (b & 1) {
+			res = res * a % MOD;
+		}
+		a = a * a % MOD;
+		b >>= 1;
+	}
+	return res;
+}
+
+int mdiv(int a, int b) {
+	return a * mpow(b, MOD - 2) % MOD;
+}
+
+int32_t main() {
+	setup();
+
+	int n, m;
+	input(n, m);
+
+	vi v(n * m + 1, 1);
+	rep(i, 0, n) {
+		vi a(m);
+		arrput(a);
+
+		vi f(n * m + 1, 0);
+		for (int j : a) {
+			f[j]++;
+		}
+		rep(j, 0, n * m + 1) {
+			v[j] = v[j] * (m - f[j]) % MOD;
+		}
+	}
+
+	int res = 0, z = mpow(m, n);
+	for (int j : v) {
+		res = (res + z - j + MOD) % MOD;
+	}
+	print(res);
+}
---
+++ Atcoder-Contests/ARC218/B - All Minus.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+using namespace __gnu_pbds; 
+template<typename T> inline void input(T& x) {cin >> x;}
+template<typename T, typename... S> inline void input(T& x, S&... args) {cin >> x; input(args ...);}
+template<typename T> inline void print(T x) {cout << x << '\n';}
+template<typename T, typename... S> inline void print(T x, S... args) {cout << x << ' '; print(args ...);}
+#define debug(...) cout << #__VA_ARGS__ << ": "; print(__VA_ARGS__);
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define arrput(l) for (auto &i : l) {cin >> i;}
+#define arrprint(l) for (auto i : l) {cout << i << ' ';} cout << '\n'
+#define setup() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define int long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define sz(x) ((int) (x.size()))
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> 
+typedef vector<int> vi; typedef pair<int, int> pii;
+const int MOD = (int) 1e9 + 7; //998244353;
+
+int32_t main() {
+	setup(); int tc; input(tc); while (tc--) {
+		int n;
+		input(n);
+
+		vi a(n);
+		arrput(a);
+		sort(all(a));
+
+		for (int i = n - 1; i > 0; i--) {
+			a[i] -= a[i - 1];
+		}
+
+		int x = count(all(a), 1);
+		if (x == n) {
+			print("Bob");
+			continue;
+		}
+		if (x == n - 1 and a[0] == 0) {
+			print("Alice");
+			continue;
+		}
+
+		bool flag = true;
+		rep(i, 0, n) {
+			if (a[i] > 1) {
+				break;
+			}
+			if (a[i] == 1) {
+				flag = !flag;
+			}
+			if (i < n - 1 and a[i + 1] == 0) {
+				break;
+			}
+			flag = !flag;
+		}
+
+		if (flag) {
+			print("Alice");
+		}
+		else {
+			print("Bob");
+		}
+	}
+}
